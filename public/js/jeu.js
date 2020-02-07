eadba06aad008852a3cff1436b80e066a8e0c03f
v-randranto <verorand@yahoo.com>
2020-02-07 18:28:48 +0100

    contrôle session (en local)

--- public/js/jeu.js
+++ public/js/jeu.js
@@ -7,20 +7,21 @@ window.addEventListener("DOMContentLoaded", function () {
     /**
      *  gestion du jeu
      */
-    const HTMLgameIntro = document.getElementById('gameIntro');
-    const HTMLplayBtn = document.getElementById('playBtn');
+    const HTMLGameIntro = document.getElementById('gameIntro');
+    const HTMLPlayBtn = document.getElementById('playBtn');
 
-    const HTMLgamesList = document.getElementById('gamesList');
-    const HTMLinitGameBtn = document.getElementById('initGameBtn');
+    const HTMLGamesList = document.getElementById('gamesList');
+    const HTMLInitGameBtn = document.getElementById('initGameBtn');
+    const HTMLEnterGameBtn = document.getElementById('enterGameBtn');
 
-    const HTMLquestion = document.getElementById('question');
+    const HTMLQuestion = document.getElementById('question');
     const HTMLAnswserForm = document.getElementById('answerForm');
 
     /**
      *  gestion de l'entrée dans le jeu quand l'utilisateur clique sur 'jouer'
      */
 
-    HTMLplayBtn.addEventListener('click', function () {
+    HTMLPlayBtn.addEventListener('click', function () {
         console.log('> click play button')
         const ioSocket = io("http://localhost:8080");
 
@@ -28,20 +29,26 @@ window.addEventListener("DOMContentLoaded", function () {
             console.log('> connect socket.io ');
 
             // afficher la liste des derniers jeux   
-            HTMLgameIntro.style.display = "none";
-            HTMLgamesList.style.visibility = "visible";
+            HTMLGameIntro.style.display = "none";
+            HTMLGamesList.style.visibility = "visible";
 
-            HTMLinitGameBtn.addEventListener('click', function () {
+            HTMLInitGameBtn.addEventListener('click', function () {
                 console.log('> click on game init')
                 // demander au serveur la création d'une partie
                 ioSocket.emit("createGame", "createGameObject");
+            })
+
+            HTMLEnterGameBtn.addEventListener('click', function () {
+                console.log('> click on game init')
+                // demander au serveur la création d'une partie
+                ioSocket.emit("addPlayer", "playerObject");
             })
 
             ioSocket.on("question", function (question) {
                 //afficher la question
                 //HTMLinitGameBtn.style.disabled;
 
-                HTMLquestion.style.visibility = "visible";
+                HTMLQuestion.style.visibility = "visible";
                 console.log('> question ', question);
                 document.getElementById('wordLength').innerHTML = `Mot de ${question.wordLength} lettres.`;
                 document.getElementById('definition').innerHTML = `Définition : ${question.definition}`;
--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const options = {
   resave: false
 };
 
+const users = [
+  { pseudo: 'vero', pwd: '1234' },
+  { pseudo: 'riton', pwd: '1234' }
+]
+
 /* Déclaration du moteur de template */
 app.set('view engine', 'pug');
 
@@ -32,12 +37,12 @@ app.set('view engine', 'pug');
 app.use(expressSession(options));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
-// app.use('/css', express.static(__dirname + '/public/css'));
-// app.use('/img', express.static(__dirname + '/public/img'));
-//app.use('/js', express.static(__dirname + '/public/js'));
 
 let interpolations = {}; // objet contenant les variables d'interpolation des fichiers .pug
-let userConnected;
+app.use(function (req, res, next) {
+  interpolations = {};
+  next();
+});
 
 app.all('/', function (req, res) {
   console.log('>app.use / ', req.originalUrl)
@@ -47,11 +52,40 @@ app.all('/', function (req, res) {
 
 app.post('/login', function (req, res) {
   console.log('> /login');
-  interpolations.pseudo = req.body.pseudo
-  // accès DB pour rechercher le pseudo dans la collection 'users'
-  // si trouvé contrôle du mot de passe
-  // si mot de passe ok alors création d'une session et envoi de la page jeu.html
-  res.render('jeu', interpolations);
+
+  // L'utilisateur.rice est déjà connecté.e
+  if (req.session.pseudo) {
+    console.log('> session existante pour ', req.body.pseudo)
+    res.render('jeu', interpolations);
+  } else {
+    console.log('> pas de session pour ', req.session.pseudo)
+    // accès DB pour rechercher le pseudo dans la collection 'users'   
+    // si trouvé contrôle du mot de passe
+    // si mot de passe ok alors création d'une session et envoi de la page jeu.html 
+    // TODO Code de test à supprimer
+    let userFound = false;
+    let validCredentials = false;
+    users.forEach(function (user) {
+      if (user.pseudo === req.body.pseudo) {
+        userFound = true;
+        if (user.pwd === req.body.pwd) {
+          validCredentials = true;
+        }
+      }
+    });
+
+    if (validCredentials) {
+      console.log('> contrôle identifiants OK')
+      req.session.pseudo = req.body.pseudo;
+      interpolations.pseudo = req.body.pseudo;
+      res.render('jeu', interpolations);
+    } else {
+      console.log('> contrôle identifiants KO')
+      interpolations.msgError = "Vos identifiants sont incorrects"
+      res.render('index', interpolations);  // TODO faire un redirect ????
+    }
+  }
+
 })
 
 /**
@@ -64,7 +98,7 @@ app.use(function (req, res, next) {
   if (res.statusCode != 403 && res.statusCode != 503) {
     res.statusCode = 404;
   }
-  res.render('erreur', interpolations);
+  res.render('error', interpolations);
 });
 
 const HTTPServer = app.listen(8080, function () {
@@ -80,8 +114,6 @@ const ioServer = io(HTTPServer);
 
 const gamesInProgress = [];
 const connectedPlayers = [];
-
-
 
 ioServer.on("connect", function (ioSocket) {
 
@@ -100,35 +132,36 @@ ioServer.on("connect", function (ioSocket) {
   // Quand la partie est finie et on fait la somme des scores de chacun. Celui qui a le plus de points a gagné
   // - mise à jour de la partie en base avec les scores de chacun
   // - la partie est supprimée de la tables "gamesInProgress"
-  
+
   // quand le dernier tour est terminé, faire le total de score de chacun des joueurs et mettre à jour la collection "games"
   const dicoSelection = {
-    word : "abort",
-    definition : "Interrompre un process."
+    word: "abort",
+    definition: "Interrompre un process."
   }
-  const question = {  
-    wordLength : dicoSelection.word.length,
-    definition : dicoSelection.definition
+  const question = {
+    wordLength: dicoSelection.word.length,
+    definition: dicoSelection.definition
   }
-  
+
+
   ioSocket.on("createGame", function (createGameObject) {
     console.log("> server create game")
     // création d'une partie avec en 1er joueur celui qui l'a initié 
     // pour le test je sers la question ici. TODO à supprimer
-   
-    ioServer.emit("question", question ); // à supprimer 
+
+    ioServer.emit("question", question); // à supprimer 
   });
   ioSocket.on("enterGame", function (addPlayer) {
     // ajout du 2è joueur à une partie
     // création d'un tour avec un mot à deviner
     // servir la question au client 
-    
+
     ioServer.emit("question", question);
-  });  
+  });
 
   ioSocket.on("answer", function (answer) {
     // du code
-    const result = { 
+    const result = {
       status: false,
       message: "Mauvaise réponse"
     };
@@ -136,8 +169,8 @@ ioServer.on("connect", function (ioSocket) {
     if (answer.toUpperCase() === dicoSelection.word.toUpperCase()) {
       //terminer le tour : annoncer le gagnant, enregistrer le tour, créer un nouveau tour
       result.status = true;
-      result.message = "Bonne réponse !"; 
-      result.scores = {}     
+      result.message = "Bonne réponse !";
+      result.scores = {}
     }
     ioServer.emit("answerChecked", result);
   });
