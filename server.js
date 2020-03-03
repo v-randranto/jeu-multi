e86d836d16fb9139bdbf829e5e5e2f4e57aefb80
v-randranto <verorand@yahoo.com>
2020-03-03 20:43:07 +0100

    cors http

--- server.js
+++ server.js
@@ -38,6 +38,13 @@ const options = {
   resave: false
 };
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(expressSession(options));
 
 app.use(bodyParser.urlencoded({ extended: false }));
